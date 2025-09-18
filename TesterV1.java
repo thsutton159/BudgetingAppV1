95ba54bdca813b083793015e05be47210ef7d9e8
Thomas Sutton <157766439+thsutton159@users.noreply.github.com>
2025-09-17 22:45:47 -0400

    Update TesterV1.java
    Updated the menu so it now sits within a loop.

--- TesterV1.java
+++ TesterV1.java
@@ -1,6 +1,6 @@
 /* tj sutton
  * Budgeting app v1 - testerv1
- * Last updated : 6.29.2025
+ * Last updated : 9.17.2025
  */
 
 package BankFunctions;
@@ -16,6 +16,7 @@ public class TesterV1 {
 	public static void main(String [] args) {
 		
 		BankAccount acc1 = new BankAccount();
+		int menuOption;
 		double checking, saving, deposit;
 		Scanner myScan = new Scanner(System.in);
 		
@@ -34,11 +35,60 @@ public class TesterV1 {
 		//System.out.println("Please enter the deposit amount");
 		//deposit = myScan.nextDouble();
 		
-		//acc1.deposit(deposit);
-		acc1.displayBalances();
-		acc1.displayExpenses();
-		myScan.close();
+		do {
+			acc1.displayBalances();
+			System.out.println("Which of the following actions would you like to take in your account?");
+			System.out.println("Please enter the number for your option.");
+			//System.out.println("1. Display Account Balances.");
+			//System.out.println("2. Display Expenses.");
+			System.out.println("1. Deposit.");
+			System.out.println("2. Withdrawal.");
+			System.out.println("3. Distrubite Funds");
+			//System.out.println("5. Make a Savings Plan");
+			System.out.println("0. Exit");
+			System.out.println();
+			menuOption = myScan.nextInt();
+			
+			switch(menuOption){
+				case 1:
+					System.out.println("Please enter your deposit amount");
+					System.out.println();
+					acc1.deposit(myScan.nextDouble());
+					acc1.distribute();
+					break;
+				case 2:
+					System.out.println("Please enter your withdrawl amount:");
+					System.out.println();
+					acc1.withdrawal(myScan.nextDouble());
+					acc1.distribute();
+					break;
+				case 3: 
+					acc1.distribute();
+					break;
+				case 0:
+					menuOption = 0;
+					break;
+				default:
+					System.out.println("Please enter a valid option.");
+					System.out.println();
+					break;
+			}
+		
+		}while(menuOption != 0);
+		
+		System.out.println("Thank you, and have a nice day!");
+		/*
+			//acc1.deposit(deposit);
+			acc1.displayBalances();
+			System.out.println();
+			acc1.displayExpenses();
+			System.out.println();
+		*/
+		
+			myScan.close();
+		
 		
 	}
 	
 }
+
